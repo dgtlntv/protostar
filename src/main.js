ddc24364452c93826d6d3cb9c299ebb2839305a1
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2024-07-08 16:15:30 +0200

    add xterm css back in

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { initializeTerminal } from "./components/Terminal.js"
 import "./styles.css"
+import "xterm/css/xterm.css"
 
 document.addEventListener("DOMContentLoaded", initializeTerminal)
